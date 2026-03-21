a9baf4f394f3dc7919e55fc90a0698347659adf7
YuanHHHH <haoyuanhuang@MacBook-Air-2.local>
2026-03-21 17:13:56 +0800

    接入coingecko，实现币价查询，数据持久化

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
-from src.tools.price import get_crypto_price
+from src.tools.price import get_crypto_price,get_multiple_prices
 from src.utils.config import SUPPORTED_COINS
 
-for coin in SUPPORTED_COINS:
-    result = get_crypto_price(coin)
-    print(result)
+result = get_crypto_price('bitcoin')
+print(result)
+
+results = get_multiple_prices(["bitcoin", "ethereum", "solana"])
+for r in results:
+    print(r)
--- src/tools/price.py
+++ src/tools/price.py
@@ -1,2 +1,100 @@
+from decimal import MIN_EMIN, MAX_EMAX
+
+import requests
+import os
+import json
+import datetime
+
+def retry(func):
+    def wrapper(*args, **kwargs):
+        max_times = 3
+        for i in range(max_times):
+            try:
+                res = func(*args, **kwargs)
+                return res
+            except Exception as e:
+                print(f"第{i+1}次失败：{e}")
+    return wrapper
+
+@retry
 def get_crypto_price(symbol: str) -> dict:
-    return {"symbol": symbol, "price": 60000, "change_24h": 2.5}
+    api_key = os.environ.get("CG_API","CG-PT7xKULi3XkZ5z3ung8Yho3K")
+    url = "https://api.coingecko.com/api/v3/simple/price"
+    params = {
+        "ids": symbol,
+        "vs_currencies": "usd",
+        "include_24hr_change": "true",
+    }
+    headers = {
+        "x-cg-demo-api-key": api_key,
+    }
+    coin_price = requests.get(url, params=params, headers=headers)
+
+    data = coin_price.json()
+    if symbol in data:
+        coin_data = data[symbol]
+        res = {
+            "symbol":symbol,
+            "price": coin_data.get("usd",0),
+            "change_24h": coin_data.get("usd_24h_change",0)
+        }
+        save_to_history("/Users/haoyuanhuang/PycharmProjects/crypto-agent/data/price_history.jsonl", res)
+        return res
+    else:
+        return None
+
+@retry
+def get_multiple_prices(coin_list) -> dict:
+    api_key = os.environ.get("CG_API", "CG-PT7xKULi3XkZ5z3ung8Yho3K")
+    url = "https://api.coingecko.com/api/v3/simple/price"
+    ids_list = ",".join(coin_list)
+    params = {
+        "ids": ids_list,
+        "vs_currencies": "usd",
+        "include_24hr_change": "true",
+    }
+    headers = {
+        "x-cg-demo-api-key": api_key,
+    }
+    coin_price = requests.get(url, params=params, headers=headers)
+
+    data = coin_price.json()
+    res = []
+
+    for coin in coin_list:
+        if coin in data:
+            coin_data = data[coin]
+            para = {
+                    "symbol": coin,
+                    "price": coin_data.get("usd",0),
+                    "change_24h": coin_data.get("usd_24h_change",0)
+                }
+            res.append(para)
+            save_to_history("/Users/haoyuanhuang/PycharmProjects/crypto-agent/data/price_history.jsonl",para)
+    return res
+
+def save_to_history(file,record):
+    record["time"] = str(datetime.datetime.now())
+    os.makedirs(os.path.dirname(file), exist_ok=True)
+    with open(file, "a") as f:
+        f.write(json.dumps(record) + "\n")
+        print("loaded successfully")
+
+def load_price_history(file):
+    try:
+        with open(file, "r") as f:
+            records = [json.loads(line) for line in f if line.strip()]
+            return records
+    except Exception as e:
+        print(e)
+        return []
+
+def analyze_history(file,coin):
+    records = load_price_history(file)
+    max_price = float('-inf')
+    min_price = float('inf')
+    for record in records:
+        if record["symbol"] == coin:
+            max_price = max(record["price"], max_price)
+            min_price = min(record["price"], min_price)
+    return max_price, min_price
