18873704cefc6b54840bb8bb0e802e3d01979bb5
YuanHHHH <haoyuanhuang@MacBook-Air-2.local>
2026-03-28 11:39:59 +0800

    Day3: 多轮对话实现, 支持连续追问和上下文保持

--- src/llm.py
+++ src/llm.py
@@ -12,34 +12,41 @@ headers = {
     "Content-Type": "application/json",
 }
 
-coin_price = get_crypto_price("bitcoin")
-
+coin_quest=input("请输入你想询问的代币\n")
+coin_price = get_crypto_price(coin_quest)
 data = {
     "model":"MiniMax-M2.7",
     "messages": [
-        {"role": "system", "content": "你是一个加密货币数据分析助手。你的任务是根据提供的价格数据进行客观的技术分析，包括趋势判断、波动率分析、关键价位识别。你只做数据层面的分析，不提供买卖建议。此外，请你回答时返回JSON格式"},
+        {"role": "system", "content": "你是一个加密货币数据分析助手。你的任务是根据提供的价格数据进行客观的技术分析，包括趋势判断、波动率分析、关键价位识别。你只做数据层面的分析，不提供买卖建议。"},
         {"role": "user", "content": f"比特币现在的价格为{coin_price}"},
     ],
     "max_tokens": 1000,
     "temperature": 0.7
 }
+while True:
 
-response = requests.post(url=mm_BASE_URL, headers=headers, json=data)
-result = response.json()
-content = result["choices"][0]["message"]["content"]
+    response = requests.post(url=mm_BASE_URL, headers=headers, json=data)
+    result = response.json()
+    content = result["choices"][0]["message"]["content"]
 
-if "<think>" in content:
-    content = content.split("</think>")[-1].strip()
-content = content.replace("```json", "").replace("```", "").strip()
-start = content.find("{")
-end = content.rfind("}") + 1
-if start != -1 and end > start:
-    content = content[start:end]
+    if "<think>" in content:
+        content = content.split("</think>")[-1].strip()
+    content = content.replace("```json", "").replace("```", "").strip()
 
-try:
-    parsed = json.loads(content)
-    print("JSON 解析成功")
-    print(json.dumps(parsed, indent=2, ensure_ascii=False))
-except json.JSONDecodeError as e:
-    print(f"JSON 解析失败: {e}")
-    print(f"原始内容: {content}")
+    print(content)
+
+    data["messages"].append(
+        {
+            "role": "assistant",
+            "content": content
+        }
+    )
+    new_user_input = input("请输入你想问的问题")
+    if new_user_input=="没有了":
+        break
+    data["messages"].append(
+        {
+            "role": "user",
+            "content": new_user_input
+        }
+    )
